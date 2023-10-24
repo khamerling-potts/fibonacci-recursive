8bd75233964c4b9adf2c40f06dadf5a9492c4d89
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-24 09:55:29 -0400

    done

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,9 @@
 function fibonacci(n) {
-  // type your code here
+  // base case
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
@@ -16,9 +20,24 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("expecting 4181");
+  console.log(fibonacci(19));
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+Problem: find the nth number in the fibonacci series and return it
+base case: if n<2, return n
+return fibonacci(n-1) + fibonacci(n-2)
+[0, 1, 1, 2, 3], n=4
+fib(3) + fib(2)
+fib(2) + fib(1)  |  fib(1) + fib(0)
+fib(1) + fib(0) + 1  |  1 + 0
+1 + 0 + 1  |  1
+2  |  1
+3
+*/
